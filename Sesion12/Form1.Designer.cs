470a4258c9fd65a612ddd8de69a4a2e7258a2720
ElAiwan <chavarriaivan97@gmail.com>
2024-09-27 10:50:01 -0600

    Agregué el Product Form

--- Sesion12/Form1.Designer.cs
+++ Sesion12/Form1.Designer.cs
@@ -28,13 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbCode = new System.Windows.Forms.Label();
+            this.lbName = new System.Windows.Forms.Label();
+            this.lbDescription = new System.Windows.Forms.Label();
+            this.lbPrice = new System.Windows.Forms.Label();
+            this.tbId = new System.Windows.Forms.TextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbPrice = new System.Windows.Forms.TextBox();
+            this.tbDescription = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.gbRegisters = new System.Windows.Forms.GroupBox();
+            this.dgvProducts = new System.Windows.Forms.DataGridView();
+            this.gbRegisters.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbCode
+            // 
+            this.lbCode.AutoSize = true;
+            this.lbCode.Location = new System.Drawing.Point(41, 40);
+            this.lbCode.Name = "lbCode";
+            this.lbCode.Size = new System.Drawing.Size(51, 16);
+            this.lbCode.TabIndex = 0;
+            this.lbCode.Text = "Código";
+            this.lbCode.Click += new System.EventHandler(this.lbCodigo_Click);
+            // 
+            // lbName
+            // 
+            this.lbName.AutoSize = true;
+            this.lbName.Location = new System.Drawing.Point(41, 76);
+            this.lbName.Name = "lbName";
+            this.lbName.Size = new System.Drawing.Size(56, 16);
+            this.lbName.TabIndex = 1;
+            this.lbName.Text = "Nombre";
+            // 
+            // lbDescription
+            // 
+            this.lbDescription.AutoSize = true;
+            this.lbDescription.Location = new System.Drawing.Point(41, 117);
+            this.lbDescription.Name = "lbDescription";
+            this.lbDescription.Size = new System.Drawing.Size(79, 16);
+            this.lbDescription.TabIndex = 2;
+            this.lbDescription.Text = "Descripción";
+            // 
+            // lbPrice
+            // 
+            this.lbPrice.AutoSize = true;
+            this.lbPrice.Location = new System.Drawing.Point(41, 153);
+            this.lbPrice.Name = "lbPrice";
+            this.lbPrice.Size = new System.Drawing.Size(46, 16);
+            this.lbPrice.TabIndex = 3;
+            this.lbPrice.Text = "Precio";
+            // 
+            // tbId
+            // 
+            this.tbId.Location = new System.Drawing.Point(143, 34);
+            this.tbId.Name = "tbId";
+            this.tbId.Size = new System.Drawing.Size(190, 22);
+            this.tbId.TabIndex = 4;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(143, 73);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(287, 22);
+            this.tbName.TabIndex = 5;
+            // 
+            // tbPrice
+            // 
+            this.tbPrice.Location = new System.Drawing.Point(143, 147);
+            this.tbPrice.Name = "tbPrice";
+            this.tbPrice.Size = new System.Drawing.Size(190, 22);
+            this.tbPrice.TabIndex = 7;
+            // 
+            // tbDescription
+            // 
+            this.tbDescription.Location = new System.Drawing.Point(143, 114);
+            this.tbDescription.Name = "tbDescription";
+            this.tbDescription.Size = new System.Drawing.Size(616, 22);
+            this.tbDescription.TabIndex = 6;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(387, 145);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(106, 32);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Agregar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // gbRegisters
+            // 
+            this.gbRegisters.Controls.Add(this.dgvProducts);
+            this.gbRegisters.Location = new System.Drawing.Point(44, 183);
+            this.gbRegisters.Name = "gbRegisters";
+            this.gbRegisters.Size = new System.Drawing.Size(718, 240);
+            this.gbRegisters.TabIndex = 9;
+            this.gbRegisters.TabStop = false;
+            // 
+            // dgvProducts
+            // 
+            this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProducts.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvProducts.Location = new System.Drawing.Point(3, 18);
+            this.dgvProducts.Name = "dgvProducts";
+            this.dgvProducts.RowHeadersWidth = 51;
+            this.dgvProducts.RowTemplate.Height = 24;
+            this.dgvProducts.Size = new System.Drawing.Size(712, 219);
+            this.dgvProducts.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gbRegisters);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tbPrice);
+            this.Controls.Add(this.tbDescription);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.tbId);
+            this.Controls.Add(this.lbPrice);
+            this.Controls.Add(this.lbDescription);
+            this.Controls.Add(this.lbName);
+            this.Controls.Add(this.lbCode);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.gbRegisters.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbCode;
+        private System.Windows.Forms.Label lbName;
+        private System.Windows.Forms.Label lbDescription;
+        private System.Windows.Forms.Label lbPrice;
+        private System.Windows.Forms.TextBox tbId;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbPrice;
+        private System.Windows.Forms.TextBox tbDescription;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.GroupBox gbRegisters;
+        private System.Windows.Forms.DataGridView dgvProducts;
     }
 }
 
